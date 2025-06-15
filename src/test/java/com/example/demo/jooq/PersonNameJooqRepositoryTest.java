448ae65ac21735468bc987f2c1b85afe0bdc199b
OlegCheban <oleg.cheban1989@gmail.com>
2025-06-15 12:16:36 +0300

    modify jooq test

--- src/test/java/com/example/demo/jooq/PersonNameJooqRepositoryTest.java
+++ src/test/java/com/example/demo/jooq/PersonNameJooqRepositoryTest.java
@@ -1,32 +1,50 @@
 package com.example.demo.jooq;
 
 import com.example.demo.dao.jooq.PersonJooqRepository;
+import org.jooq.DSLContext;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import static org.jooq.codegen.demo.Sequences.PERSON_SEQ;
+import static org.jooq.codegen.demo.Tables.PERSON;
+
 @Testcontainers
 @DataJpaTest
 @ContextConfiguration(classes = {JooqConfig.class})
+@ActiveProfiles("jooq")
 public class PersonNameJooqRepositoryTest {
 
     @Autowired
     private PersonJooqRepository personJooqRepository;
 
+    @Autowired
+    private DSLContext dslContext;
+
     @Container
     @ServiceConnection
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
 
+    @BeforeEach
+    void setUp() {
+        dslContext.insertInto(PERSON)
+                .values(PERSON_SEQ.nextval(), "John Doe", 30)
+                .values(PERSON_SEQ.nextval(), "Jane Smith", 25)
+                .execute();
+    }
+
     @Test
     void shouldSaveAndRetrievePerson(){
         var persons = personJooqRepository.getPersonByName("John Doe");
-
-        Assertions.assertTrue(persons.isEmpty());
+        Assertions.assertFalse(persons.isEmpty());
+        Assertions.assertEquals(1, persons.size());
     }
 }
