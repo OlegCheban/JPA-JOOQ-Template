ccb2accde6323a966daea8417ede44a8f6cbc2bd
Oleg Cheban <oleg.cheban1989@mail.ru>
2026-02-21 13:38:19 +0300

    jooq: configutation enhancement

--- src/main/java/com/example/demo/config/ExceptionTranslator.java
+++ src/main/java/com/example/demo/config/ExceptionTranslator.java
@@ -2,15 +2,29 @@ package com.example.demo.config;
 
 import org.jooq.ExecuteContext;
 import org.jooq.ExecuteListener;
-import org.jooq.SQLDialect;
-import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
 import org.springframework.jdbc.support.SQLExceptionTranslator;
 
 public class ExceptionTranslator implements ExecuteListener {
 
+    private final SQLExceptionTranslator translator;
+
+    public ExceptionTranslator(SQLExceptionTranslator translator) {
+        this.translator = translator;
+    }
+
     public void exception(ExecuteContext context) {
-        SQLDialect dialect = context.configuration().dialect();
-        SQLExceptionTranslator translator = new SQLErrorCodeSQLExceptionTranslator(dialect.name());
-        context.exception(translator.translate("Access database using Jooq", context.sql(), context.sqlException()));
+        if (context.sqlException() == null) {
+            return;
+        }
+
+        var translated = translator.translate(
+                "Access database using jOOQ",
+                context.sql(),
+                context.sqlException()
+        );
+
+        if (translated != null) {
+            context.exception(translated);
+        }
     }
 }
--- src/main/java/com/example/demo/config/PersistenceContext.java
+++ src/main/java/com/example/demo/config/PersistenceContext.java
@@ -1,5 +1,6 @@
 package com.example.demo.config;
 
+import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DataSourceConnectionProvider;
 import org.jooq.impl.DefaultConfiguration;
@@ -7,31 +8,43 @@ import org.jooq.impl.DefaultDSLContext;
 import org.jooq.impl.DefaultExecuteListenerProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
+import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
+import org.springframework.jdbc.support.SQLExceptionTranslator;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceContext {
-    private final DataSourceConnectionProvider dataSourceConnectionProvider;
 
-    public PersistenceContext(DataSourceConnectionProvider dataSourceConnectionProvider) {
-        this.dataSourceConnectionProvider = dataSourceConnectionProvider;
+    @Bean
+    public DataSourceConnectionProvider connectionProvider(DataSource dataSource) {
+        return new DataSourceConnectionProvider(new TransactionAwareDataSourceProxy(dataSource));
     }
 
     @Bean
-    public ExceptionTranslator exceptionTransformer() {
-        return new ExceptionTranslator();
+    public SQLExceptionTranslator sqlExceptionTranslator(DataSource dataSource) {
+        return new SQLErrorCodeSQLExceptionTranslator(dataSource);
     }
 
     @Bean
-    public DefaultConfiguration configuration() {
+    public ExceptionTranslator exceptionTranslator(SQLExceptionTranslator translator) {
+        return new ExceptionTranslator(translator);
+    }
+
+    @Bean
+    public DefaultConfiguration configuration(
+            DataSourceConnectionProvider connectionProvider,
+            ExceptionTranslator exceptionTranslator) {
         DefaultConfiguration JooqConfiguration = new DefaultConfiguration();
-        JooqConfiguration.set(dataSourceConnectionProvider);
-        JooqConfiguration.set(new DefaultExecuteListenerProvider(exceptionTransformer()));
+        JooqConfiguration.set(connectionProvider);
         JooqConfiguration.set(SQLDialect.POSTGRES);
+        JooqConfiguration.set(new DefaultExecuteListenerProvider(exceptionTranslator));
         return JooqConfiguration;
     }
 
     @Bean
-    public DefaultDSLContext dsl() {
-        return new DefaultDSLContext(configuration());
+    public DSLContext dslContext(org.jooq.Configuration jooqConfiguration) {
+        return new DefaultDSLContext(jooqConfiguration);
     }
 }
